a9f941a0d7e31c9eaf03a1887c7cbd8631b8e4bf
k_stein <karlasteinbrink@gmail.com>
2026-01-06 12:03:49 +0000

    Secure SECRET_KEY with environment variable

--- mentor_market/settings.py
+++ mentor_market/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u975_t6yo@0vsscng+zqyj#)04mam2vbde6=@v8ra#399v)q^_'  # noqa: E501
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
