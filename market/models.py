1cbbbbd8208ce86b3476e2d5a03e3bd1a110da53
k_stein <karlasteinbrink@gmail.com>
2026-01-06 15:33:47 +0000

    Add Profile database structure and wire to admin

--- market/models.py
+++ market/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    excerpt = models.TextField(blank=True)
+    bio = models.TextField()
+    experience = models.TextField()
+    specialism = models.TextField()
+    member_since = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
