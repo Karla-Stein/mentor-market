348e061cf968135a4058094377d833ae0bb2980b
k_stein <karlasteinbrink@gmail.com>
2026-01-12 15:04:49 +0000

    Add profile detail view, routing and link from index page

--- market/urls.py
+++ market/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ProfileList.as_view(), name='home')
+    path('', views.ProfileList.as_view(), name='home'),
+    path('<slug:slug>/', views.profile_detail, name='profile_detail'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Profile
 
@@ -9,3 +9,27 @@ class ProfileList(generic.ListView):
     queryset = Profile.objects.filter(status=1).order_by("-member_since")
     template_name = "market/index.html"
     paginate_by = 8
+
+
+def profile_detail(request, slug):
+    """
+    Display an individual :model:`market.Profile`.
+
+    **Context**
+
+    ``profile``
+        An instance of :model:`market.Profile`.
+
+    **Template:**
+
+    :template:`market/profile_detail.html`
+    """
+    queryset = Profile.objects.filter(status=1)
+    profile = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "market/profile_detail.html",
+        {"profile": profile}
+    )
+
