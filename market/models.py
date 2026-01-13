cf894a40b239ed0b8e275f74ad35693c7e7e3e4a
k_stein <karlasteinbrink@gmail.com>
2026-01-13 11:24:18 +0000

    Add TimeSlot model and migrations and add to Admin

--- market/models.py
+++ market/models.py
@@ -22,7 +22,27 @@ class Profile(models.Model):
     updated_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-member_since"]  # newest Profile is displayed first  # noqa 501
-    
+        ordering = ["-member_since"]  # newest Profile is displayed first
+         
     def __str__(self):
         return f"Profile of {self.user}"
+
+
+AVAILABILITY_STATUS = ((0, "Open"), (1, "Booked"))
+
+
+class TimeSlot(models.Model):
+    mentor = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="availability"
+    )
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    availability_status = models.IntegerField(choices=AVAILABILITY_STATUS, default=0)  # noqa 501
+
+    class Meta:
+        unique_together = ("mentor", "date", "start_time", "end_time")
+
+    def __str__(self):
+        return f"{self.mentor.name} is available on {self.date} from {self.start_time} to {self.end_time}"  # noqa 501
+    
