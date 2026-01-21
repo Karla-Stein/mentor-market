50850c6644f3a79243573c466a9398b932f781f6
k_stein <karlasteinbrink@gmail.com>
2026-01-21 13:00:59 +0000

    Enable Claudinary image storage

--- market/models.py
+++ market/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -13,6 +14,7 @@ class Profile(models.Model):
     )
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(max_length=120)
     bio = models.TextField()
     experience = models.TextField()
