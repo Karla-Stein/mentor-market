d3a848aa23479e29cb0365db30f6266282cb800c
k_stein <karlasteinbrink@gmail.com>
2026-02-14 14:13:40 +0000

    Resolve accessibility warning on modal close

--- staticfiles/js/profile_delete.js
+++ staticfiles/js/profile_delete.js
@@ -8,3 +8,13 @@ const modal = new bootstrap.Modal(modalDelete);
 deleteButton.addEventListener("click", (e) => {
         modal.show();
 });
+
+/* 
+ defuse focus before hiding kicks in to fix the console warning for the modal.
+ Reused from previous project.
+*/
+modalDelete.addEventListener("hide.bs.modal", () => {
+    if (modalDelete.contains(document.activeElement)) {
+        document.activeElement.blur();
+    }
+});
